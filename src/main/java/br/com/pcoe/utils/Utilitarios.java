8db3304346d985b3d14ecb4ff8efcab1456ceb70
victorsena19 <victor_sena19@hotmail.com>
2025-05-27 16:42:45 -0400

    implementado funcoes: validarPermissaoOuLancar,validarSomenteAdmin,validarAcessoAdminParaCaixas

--- src/main/java/br/com/pcoe/utils/Utilitarios.java
+++ src/main/java/br/com/pcoe/utils/Utilitarios.java
@@ -1,19 +1,27 @@
 package br.com.pcoe.utils;
 
+import br.com.pcoe.exceptions.MensagemException;
 import br.com.pcoe.model.Caixa;
 import br.com.pcoe.model.Usuario;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 /**
  * Classe utilitária para acesso ao usuário logado e permissões.
 **/
 @Component
-public class Utilitarios {
+public final class Utilitarios {
 
     public Usuario getUsuarioLogado(){
-        return (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !(auth.getPrincipal() instanceof Usuario)){
+            throw new IllegalStateException("Usuário não autenticado");
+        }
+        return (Usuario) auth.getPrincipal();
     }
 
     public boolean isAdmin(Usuario usuario){
@@ -24,6 +32,23 @@ public class Utilitarios {
     public boolean naoEhCriadorNemAdmin(Caixa caixa){
         Usuario usuarioLogado = getUsuarioLogado();
         return !usuarioLogado.getId().equals(caixa.getUsuario().getId()) && !isAdmin(usuarioLogado);
+    }
+
+    public void validarPermissaoOuLancar(Caixa caixa, String mensagemErro){
+        if(naoEhCriadorNemAdmin(caixa)) {
+            throw new MensagemException(mensagemErro);
+        }
+    }
+    public void validarSomenteAdmin(Caixa caixa, Usuario usuario){
+        if (!isAdmin(usuario)){
+            validarPermissaoOuLancar(caixa, "Somente Admin pode acessar esse recurso.");
+        }
+    }
+
+    public void validarAcessoAdminParaCaixas(List<Caixa> caixas){
+        if(caixas == null || caixas.isEmpty())return;
+
+        caixas.forEach(cx -> validarSomenteAdmin(cx, cx.getUsuario()));
     }
 
 }
