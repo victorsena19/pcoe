e2ad9e290309634157a9da8b4e899bd26ddb0d4c
victorsena19 <victor_sena19@hotmail.com>
2025-05-27 16:34:20 -0400

    implementado enum para personalizar mensagens de erro repetitivas

--- src/main/java/br/com/pcoe/enuns/MensagensErrosGenericas.java
+++ src/main/java/br/com/pcoe/enuns/MensagensErrosGenericas.java
@@ -1,4 +1,20 @@
 package br.com.pcoe.enuns;
 
+import lombok.Getter;
+
+@Getter
 public enum MensagensErrosGenericas {
+    PERMISSAOUSUARIONEGADA("Não foi possível %s esse caixa pois voce não é Admin ou não foi você que lançou esse caixa"),
+    IDNAOENCONTRADO("Não foi possível encontrar um %s com esse ID: ");
+    private final String erro;
+
+     MensagensErrosGenericas(String erro) {
+        this.erro = erro;
+    }
+
+    public String format(Object... args){
+         return String.format(erro, args);
+    }
+
+
 }
