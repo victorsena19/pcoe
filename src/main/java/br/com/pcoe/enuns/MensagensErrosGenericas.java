0159239b1280f787666147353662193d38647078
victorsena19 <victor_sena19@hotmail.com>
2025-05-29 09:49:07 -0400

    mudanca do nome de opcao do enum

--- src/main/java/br/com/pcoe/enuns/MensagensErrosGenericas.java
+++ src/main/java/br/com/pcoe/enuns/MensagensErrosGenericas.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum MensagensErrosGenericas {
     PERMISSAOUSUARIONEGADA("Não foi possível %s esse caixa pois voce não é Admin ou não foi você que lançou esse caixa"),
-    IDNAOENCONTRADO("Não foi possível encontrar um %s com esse ID: ");
+    IDNAOENCONTRADO("Não foi possível encontrar um(a) %s com esse ID: "),
+    ARGUMENTOJAEXISTENTE("%s já encontra-se cadastrada no sistema!");
     private final String erro;
 
      MensagensErrosGenericas(String erro) {
