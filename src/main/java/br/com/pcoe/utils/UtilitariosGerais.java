4da0287881c4f60d96674900454cd4d82f572385
victorsena19 <victor_sena19@hotmail.com>
2025-05-29 09:37:30 -0400

    Criado classe para funcoes genericas para reaproveitamento de codigo

---
+++ src/main/java/br/com/pcoe/utils/UtilitariosGerais.java
@@ -0,0 +1,16 @@
+package br.com.pcoe.utils;
+
+import br.com.pcoe.enuns.MensagensErrosGenericas;
+import br.com.pcoe.exceptions.MensagemException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public final class UtilitariosGerais {
+    public <T> T buscarEntidadeId(JpaRepository<T, UUID> repository, UUID id, String nomeEntidade){
+        return repository.findById(id).orElseThrow(() ->
+                new MensagemException(MensagensErrosGenericas.IDNAOENCONTRADO.format(nomeEntidade) + id));
+    }
+}
