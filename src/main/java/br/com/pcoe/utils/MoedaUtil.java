f5aefd1c86fafedffd3e237718750ff1ee9be00e
victorsena19 <victor_sena19@hotmail.com>
2025-06-03 15:26:56 -0400

    adicionado funcao para mostrar valor numerico por escrito

--- src/main/java/br/com/pcoe/utils/MoedaUtil.java
+++ src/main/java/br/com/pcoe/utils/MoedaUtil.java
@@ -1,4 +1,41 @@
 package br.com.pcoe.utils;
 
+import com.ibm.icu.text.RuleBasedNumberFormat;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Locale;
+
 public class MoedaUtil {
+
+    public String valorPorExtenso(BigDecimal valor) {
+        if (valor == null) {
+            return "";
+        }
+
+        BigDecimal inteiro = valor.setScale(0, RoundingMode.DOWN);
+        BigDecimal centavos = valor.subtract(inteiro).movePointRight(2).setScale(0, RoundingMode.HALF_UP);
+
+        RuleBasedNumberFormat formatter = new RuleBasedNumberFormat(new Locale("pt", "BR"), RuleBasedNumberFormat.SPELLOUT);
+
+        StringBuilder valorExtenso = new StringBuilder();
+
+        long parteInteira = inteiro.longValue();
+        int parteCentavos = centavos.intValue();
+
+        if (parteInteira > 0) {
+            valorExtenso.append(formatter.format(parteInteira))
+                    .append(parteInteira == 1 ? " real" : " reais");
+        }
+
+        if (parteCentavos > 0) {
+            if (parteInteira > 0) {
+                valorExtenso.append(" e ");
+            }
+            valorExtenso.append(formatter.format(parteCentavos))
+                    .append(parteCentavos == 1 ? " centavo" : " centavos");
+        }
+
+        return valorExtenso.toString();
+    }
 }
