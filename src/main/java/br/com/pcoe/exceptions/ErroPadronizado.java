41ad8bca12a1e81ecf94865b46febdd3290b244e
victorsena19 <victor_sena19@hotmail.com>
2025-06-03 16:16:26 -0400

    adicionado funcoes para capturar excecoes

--- src/main/java/br/com/pcoe/exceptions/ErroPadronizado.java
+++ src/main/java/br/com/pcoe/exceptions/ErroPadronizado.java
@@ -1,4 +1,18 @@
 package br.com.pcoe.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErroPadronizado {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String mensagem;
+    private String caminho;
 }
--- src/main/java/br/com/pcoe/exceptions/ErroValidacao.java
+++ src/main/java/br/com/pcoe/exceptions/ErroValidacao.java
@@ -1,4 +1,19 @@
 package br.com.pcoe.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErroValidacao {
+    private LocalDateTime timestamp;
+    private int status;
+    private String mensagem;
+    private String caminho;
+    private Map<String, String> erros;
 }
