92d7d007243beecf03ad1d5f2eaa67c5f8033306
victorsena19 <victor_sena19@hotmail.com>
2025-06-09 15:44:18 -0400

    incluido funcao para buscar statusReabertura por usuario

--- src/main/java/br/com/pcoe/repository/SolicitacaoReaberturaCaixaRepository.java
+++ src/main/java/br/com/pcoe/repository/SolicitacaoReaberturaCaixaRepository.java
@@ -1,4 +1,17 @@
 package br.com.pcoe.repository;
 
-public interface SolicitacaoReaberturaCaixaRepository {
+import br.com.pcoe.enums.StatusSolicitacao;
+import br.com.pcoe.model.SolicitacaoReaberturaCaixa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SolicitacaoReaberturaCaixaRepository extends JpaRepository<SolicitacaoReaberturaCaixa, UUID> {
+    boolean existsByCaixaId(UUID id);
+
+    List<SolicitacaoReaberturaCaixa> findByStatusReabertura(StatusSolicitacao statusReabertura);
+    List<SolicitacaoReaberturaCaixa> findByStatusReaberturaAndSolicitanteId(StatusSolicitacao statusReabertura, UUID solicitante);
 }
