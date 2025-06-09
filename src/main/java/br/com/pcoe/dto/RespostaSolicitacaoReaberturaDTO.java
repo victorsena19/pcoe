503d9c3d83959f6bc4aa795bcb6d35da13ca5b51
victorsena19 <victor_sena19@hotmail.com>
2025-06-09 15:42:50 -0400

    mudanca do nome da coluna

--- src/main/java/br/com/pcoe/dto/RespostaSolicitacaoReaberturaDTO.java
+++ src/main/java/br/com/pcoe/dto/RespostaSolicitacaoReaberturaDTO.java
@@ -1,4 +1,17 @@
 package br.com.pcoe.dto;
 
+import br.com.pcoe.enums.StatusSolicitacao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RespostaSolicitacaoReaberturaDTO {
+
+    private String motivo;
+    private StatusSolicitacao statusReabertura;
+    private CaixaDTO caixa;
+    private UsuarioDTO administrador;
 }
--- src/main/java/br/com/pcoe/model/SolicitacaoReaberturaCaixa.java
+++ src/main/java/br/com/pcoe/model/SolicitacaoReaberturaCaixa.java
@@ -1,4 +1,44 @@
 package br.com.pcoe.model;
 
-public class SolicitacaoReaberturaCaixa {
+import br.com.pcoe.enums.StatusSolicitacao;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "solicitacoes_reaberturas_caixas")
+public class SolicitacaoReaberturaCaixa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Caixa caixa;
+
+    private String motivo;
+
+    private LocalDate dataSolicitacao;
+
+    private LocalDate dataResposta;
+
+    @Enumerated(EnumType.STRING)
+    private StatusSolicitacao statusReabertura;
+
+    @JsonManagedReference("solicitante")
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Usuario solicitante;
+
+    @JsonManagedReference("movimentador")
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Usuario administrador;
 }
